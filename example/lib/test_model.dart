e31e70fd481b28589886610e7fefdbbb2efae542
Justin J <justinjoyn@gmail.com>
2019-05-24 21:24:27 +0530

    token removed

--- example/lib/test_model.dart
+++ example/lib/test_model.dart
@@ -1,9 +1,9 @@
 import 'package:graphql_generator/annotation.dart';
 
 @GQLGenerator(
-    url: "https://turtle-api.herokuapp.com/graphql",
-    headerToken:
-    "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJUdXJ0bGUiLCJleHAiOjE1NjM0NzkzODYsImlhdCI6MTU1ODY0MDk4NiwiaXNzIjoiVHVydGxlIiwianRpIjoiOWUwNTVjZmYtNjNmZC00NDQyLTgxOTQtNDI2NTg4ODQwNmIwIiwibmJmIjoxNTU4NjQwOTg1LCJzdWIiOiJ1c2VyOjI4NDU2NzIzNTc2MjczMjIzNjkyNzUzMzcxNjYwMjg5IiwidHlwIjoiYWNjZXNzIn0.ceERGzJnhx4M0TJbQryhLA9l644jc_tgsGQPrlhXe8f59z3WfN5SRMDXXnmzxV-FLJhphB16Cw09so_G7wMHeQ",
+  // graphiql doesn't require a token for introspection schema
+    url: "https://turtle-api.herokuapp.com/graphiql",
+    headerToken: "",
     namespace: "T",
     types: {
       "Duration": bool,
