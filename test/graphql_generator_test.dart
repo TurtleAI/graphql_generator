29671071a0d3077dbe80eb7daadab85a7c2f6341
Justin J <justinjoyn@gmail.com>
2019-08-17 22:11:20 +0530

    Moved tests to new folder

--- test/graphql_generator_test.dart
+++ test/graphql_generator_test.dart
@@ -6,10 +6,10 @@ import 'package:graphql_generator/generator/model.dart';
 import "package:test/test.dart";
 
 void main() {
-
   test('GraphQL generation', () async {
     /// Read the test response from the file.
-    var response = await new File('test/response.json').readAsString();
+    var response = await new File('test/test_data/turtle_backend.schema.json')
+        .readAsString();
 
     var graphQLGenerator = GraphQLGenerators();
     var typesFromResponse = getTypeList(
@@ -19,18 +19,27 @@ void main() {
     var namespace = "T";
 
     /// Generate the dart code and write it to a file.
-    var resultFile = await File('test/test_generated_result.txt').writeAsString(
-        GraphQLCodeGenerators().generateDartCode(
+    var resultFile = await File('test/test_data/turtle_backend.result.txt')
+        .writeAsString(GraphQLCodeGenerators().generateDartCode(
             typesFromResponse, namespace, {}, {}, mutationTypeName));
 
     ///Read the expected result from the file as String.
     var expectedResult =
-        await new File('test/test_expected_result.txt').readAsString();
+        await new File('test/test_data/turtle_backend.output.txt')
+            .readAsString();
 
     ///Read the generated result from the file as String.
     var result = await resultFile.readAsString();
 
     ///Compare both the results.
     expect(expectedResult, result);
+  });
+
+  tearDown(() async {
+    ///Removes the result file after test
+    if (FileSystemEntity.typeSync('test/test_data/turtle_backend.result.txt') != FileSystemEntityType.notFound) {
+      var resultFile = await File('test/test_data/turtle_backend.result.txt');
+      resultFile.delete();
+    }
   });
 }
