e598015e83898865f0eba8056705300a04665ade
Justin J <justinjoyn@gmail.com>
2019-08-28 01:06:58 +0530

    Schema Test Case

--- test/graphql_generator_test.dart
+++ test/graphql_generator_test.dart
@@ -42,4 +42,57 @@ void main() {
       resultFile.delete();
     }
   });
+
+test('GraphQL generation', () async {
+    /// Read the test response from the file.
+    var graphQLGenerator = GraphQLGenerators();
+    var response = await graphQLGenerator.getSchema("https://turtle-api.herokuapp.com/graphiql", "", introspectionQuery:
+    '{"query" : "fragment FullType on __Type { kind name description fields(includeDeprecated: true) '
+              '{ name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } '
+              'inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) '
+              '{ name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } '
+              'fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } '
+              'fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType '
+              '{ kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } } '
+              'query IntrospectionQuery { __schema { queryType { name } mutationType { name } types { ...FullType } '
+              'directives { name description locations args { ...InputValue } } } } "}');
+    var typesFromResponse = getTypeList(
+        graphQLGenerator.getTypesFromResponse(response:response));
+    var mutationTypeName = graphQLGenerator.getMutationTypeNameFromResponse(
+        response: response);
+    var namespace = "T";
+
+    /// Generate the dart code and write it to a file.
+    var resultFile = await File('test/test_data/turtle_backend.schema.result.txt')
+        .writeAsString(GraphQLCodeGenerators().generateDartCode(
+            typesFromResponse, namespace, {}, {}, mutationTypeName));
+
+    ///Read the expected result from the file as String.
+    var expectedResult =
+        await new File('test/test_data/turtle_backend.schema.output.txt')
+            .readAsString();
+
+    // ///Read the generated result from the file as String.
+    var result = await resultFile.readAsString();
+
+    // ///Compare both the results.
+    expect(expectedResult, result);
+  });
+
+  tearDown(() async {
+    ///Removes the result file after test
+    if (FileSystemEntity.typeSync('test/test_data/turtle_backend.result.txt') != FileSystemEntityType.notFound) {
+      var resultFile = await File('test/test_data/turtle_backend.result.txt');
+      resultFile.delete();
+    }
+
+    if (FileSystemEntity.typeSync('test/test_data/turtle_backend.schema.result.txt') != FileSystemEntityType.notFound) {
+      var resultFile = await File('test/test_data/turtle_backend.schema.result.txt');
+      resultFile.delete();
+    }
+  });
 }
+
+
+
+
